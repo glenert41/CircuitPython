1c5073875d4daadf94e62c0df2c2bced3d89c8a5
Graham L Lenert <glenert41@ccs.local>
2019-10-10 12:43:36 -0400

    Added Proper Commenting

--- Photointerrupter_Assignment_8-30.py
+++ Photointerrupter_Assignment_8-30.py
@@ -1,6 +1,7 @@
 #Graham Lenert
 #Photointerrupter
-#This code takes a photorupters interruptions over the course of 4 seconds, and prints the amount of interruptions
+#This code takes a photorupters interruptions over the course of 4 seconds, 
+    # and prints the amount of interruptions
 
 
 
@@ -11,27 +12,32 @@ import time
 import digitalio
 import adafruit_bus_device
 
+#^ Imports Libraries
+
 photoPin = digitalio.DigitalInOut(board.D8)
 photoPin.direction = digitalio.Direction.INPUT
 photoPin.pull = digitalio.Pull.UP
+#Sets the Photointerrupter Pin as an Input and Digital Pin 8
 
-initial = time.monotonic()
 
-interrupts = -1
+initial = time.monotonic()   #Starts Timer 
+
+interrupts = -1         
 lread = True
-fread = True
+fread = True   #Sets the fread and lread variables to True
 
 
 while True:
-    now = time.monotonic()
-    if now - initial == 4:
+    now = time.monotonic() 
+    if now - initial == 4: #if it has been 4 seconds
         print("Number of interruptions: " + str(interrupts))
         initial = time.monotonic()
 
     if photoPin.value:
-        lread = True
+        lread = True   #if interrupted, turn lread to true
 
-    elif fread == lread:
-        interrupts +=1
+    elif fread == lread: #if the photointerrupter is interrupted, then uniterrupted,
+                                #count the interrupter and add 1 to the counter variable
+        interrupts +=1   
         lread = not lread
 
