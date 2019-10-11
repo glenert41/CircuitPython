29ad236eedc39ffcb4ad9ed8645d979794701359
Graham L Lenert <glenert41@ccs.local>
2019-10-11 11:24:56 -0400

    Added Proper Commenting

--- rgb.py
+++ rgb.py
@@ -1,61 +1,67 @@
+#Graham Lenert
+#september 2019
+#This code is the library for the RGB color changer. 
+# The assignment is the Classes, Objects, and Modules in canvas. 
+    #It sets an LED to the color that you call, and also has a rainbow function.
+    #Only works when you set 3 pins in the main
+
+
+
 import simpleio
 import pulseio
 import time
 
 
 
-class RGB:
+class RGB:                  #creates the class
     kind="colors"
     def __init__(self, r, g, b):
 
-
+        #sets the entered pins to PWM pins
         self.pwm_r = pulseio.PWMOut(r, frequency=1000, duty_cycle=0)
         self.pwm_g = pulseio.PWMOut(g, frequency=1000, duty_cycle=0)  #sets the PWM pins
         self.pwm_b = pulseio.PWMOut(b, frequency=1000, duty_cycle=0)    
-
-    def change_name(self, newName):
-        self.name = newName
 
     def change_pins(self, r, g, b):
         self.r = r
         self.g = g    #Sets the input pins given to us by the main
         self.b = b
 
-    def red(self):
+    def red(self):              #turns on red
         self.pwm_r.duty_cycle = 0
         self.pwm_g.duty_cycle = 2**16-1
         self.pwm_b.duty_cycle = 2**16-1
 
 
-    def blue(self):
+    def blue(self):             #turns on blue
         self.pwm_r.duty_cycle = 2**16-1
         self.pwm_g.duty_cycle = 2**16-1
         self.pwm_b.duty_cycle = 0
 
-    def magenta(self):
+    def magenta(self):          #turns on magenta (red and blue)
         self.pwm_r.duty_cycle = 0
         self.pwm_g.duty_cycle = 2**16-1
         self.pwm_b.duty_cycle = 0
 
-    def green(self):
+    def green(self):            #turns on green
         self.pwm_r.duty_cycle = 2**16-1
         self.pwm_g.duty_cycle = 0
         self.pwm_b.duty_cycle = 2**16-1
 
-    def cyan(self):
+    def cyan(self):             #turns on cyan (green and blue)
         self.pwm_r.duty_cycle = 2**16-1
         self.pwm_g.duty_cycle = 0
         self.pwm_b.duty_cycle = 0
 
-    def yellow(self):
+    def yellow(self):           #turns on yellow (green and red)
         self.pwm_r.duty_cycle = 0
         self.pwm_g.duty_cycle = 0
         self.pwm_b.duty_cycle = 2**16-1
 
     def rainbow(self, rate):
+            #rate is the delay (how fast the LED color changes)
 
-
-
+            #fades through the rainbow
         for val in range(0,2**16,128):
             self.pwm_r.duty_cycle = 0
             self.pwm_g.duty_cycle = 2**16-1 - val
